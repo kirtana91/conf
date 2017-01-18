93672ced97169eb3ab181a3e849f6ee2a1a4b220
Kirtana Yeluripati <kirtanaway@gmail.com>
2017-01-18 11:50:49 -0500

    Installing JSHint page in grunt

--- conFusion/WebContent/Gruntfile.js
+++ conFusion/WebContent/Gruntfile.js
@@ -10,6 +10,22 @@ module.exports = function (grunt) {
 
 	// Define the configuration for all the tasks
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		  // Make sure code styles are up to par and there are no obvious mistakes
+		  jshint: {
+		    options: {
+		      jshintrc: '.jshintrc',
+		      reporter: require('jshint-stylish')
+		    },
+
+		    all: {
+		      src: [
+		        'Gruntfile.js',
+		        'app/scripts/{,*/}*.js'
+		      ]
+		    }
+		  }
 
   });
 };
